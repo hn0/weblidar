169a4de99f0fdcf23bc6ec404c5699f5809cac10
Hrvoje Novosel <hrvojedotnovosel@gmail.com>
2018-03-03 18:15:02 +0100

    + reading points from file

--- src/model/model.go
+++ src/model/model.go
@@ -30,38 +30,57 @@ func CreateModel(path string) *Model {
 	defer lf.Close()
 
 	m.Numpts = lf.Header.NumberPoints
-	fmt.Printf("Processing input dataset containing %i points\n", m.Numpts)
+	if m.Numpts > 0 {
+		fmt.Printf("Processing input dataset containing %i points\n", m.Numpts)
 
-	domains := make(map[string][2]float64)
-	directions := []string{"X", "Y", "Z"}
-	r := reflect.ValueOf(lf.Header)
+		domains := make(map[string][2]float64)
+		directions := []string{"X", "Y", "Z"}
+		r := reflect.ValueOf(lf.Header)
 
-	for _, k := range directions {
-		vals := [2]float64{0, 0}
-		for i, v := range []string{"Min", "Max"} {
-			f := reflect.Indirect(r).FieldByName(v + k)
-			vals[i] = float64(f.Float())
-			fmt.Println(f)
+		for _, k := range directions {
+			vals := [2]float64{0, 0}
+			for i, v := range []string{"Min", "Max"} {
+				f := reflect.Indirect(r).FieldByName(v + k)
+				vals[i] = float64(f.Float())
+			}
+			domains[k] = vals
 		}
-		domains[k] = vals
-	}
-	fmt.Println(domains)
 
-	// read and normalize all the points
-	points := make([]point, m.Numpts)
-	fmt.Println("Reading points:\n")
+		// read and normalize all the points
+		points := make([]point, m.Numpts)
+		valid := true
+		fmt.Println("Reading points:\n")
 
-	for i := 0; i < m.Numpts; i++ {
-		// if pt, err := lf.LasPoint(i); err == nil {
-		// fmt.Println("Point:", pt)
-
-		// }
-		if i%500 == 0 {
-			fmt.Printf("\r\t %d of %d", i, m.Numpts)
+		for i := 0; i < m.Numpts; i++ {
+			if x, y, z, err := lf.GetXYZ(1000); err == nil {
+				// let say that domain will be 0 .. 1?
+				// SOMETHING THAT NEEDS A CHECK!
+				points[i].x = x / domains["X"][1]
+				points[i].y = y / domains["Y"][1]
+				points[i].z = z / domains["Z"][1]
+			} else {
+				valid = false
+			}
+			if i%500 == 0 {
+				fmt.Printf("\r\t %f%%", (float64(i)/float64(m.Numpts))*100)
+			}
 		}
-	}
+		fmt.Printf("\r\tdone \n")
 
-	m.Pts = &points
+		if valid {
+			m.Pts = &points
+			m.Valid = true
+			m.calculate_dist()
+		}
+
+	}
 
 	return m
+}
+
+func (m *Model) calculate_dist() {
+
+	fmt.Println("Classifying point cloud")
+	// TODO n by n matrix, maybe even using opencl
+	// calculate simple euclidian distances!
 }
